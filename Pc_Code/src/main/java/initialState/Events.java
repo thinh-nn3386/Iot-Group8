b96c0430600bc89939520ad7981b1e5d603a17b8
thinh-nn3386 <thinh.nn3386@gmail.com>
2021-01-22 13:57:20 +0700

    add nham :))

--- Pc_Code/src/main/java/initialState/Events.java
+++
@@ -1,9 +0,0 @@
-package initialState;
-
-interface Events {
-  //base url for Initial state (https://www.initialstate.com/) connection
-  final static String API_BASEURL = "https://groker.init.st/api/";
-
-  //API Endpoint: url for http connection
-  String getEndpoint();
-}
